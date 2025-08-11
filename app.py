1577945f1910252db5737b8687ba13c089c38cb7
Ali Nowia <142123646+AliNowia@users.noreply.github.com>
2025-08-11 18:04:04 +0300

    clean and optimization

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
-import time
+from functions import run_app
 
+# get no. of pages from user
+pages = int(input(f'How many pages to collect data ? [1 - 23]\ninput: '))
+
+# initialize the driver
 driver = webdriver.Chrome()
 
-driver.get('https://fantasy.premierleague.com/statistics')
-
-title = driver.find_element('xpath', '//h2[contains(@id, "page-title")]').text
-top_players = driver.find_elements('xpath', '//tbody//tr[contains(@role, "row")]//div[contains(@class, "uvp")]//span[contains(@class, "_5bm4v44")]')
-
-for player in top_players:
-    print(player.text, end=" | ")
-print()
-
-driver.quit()
+# run the app
+run_app(pages, driver)
